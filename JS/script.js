f5d7bc7c7f5e46ac565b305ced0589551c0bb3af
Marlon1337s <marlonlenoir1337@gmail.com>
2024-11-28 15:01:00 -0300

    refactor: improve validation logic for form fields

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,4 @@
 // Book Class: represents a book
-
 class Book {
   constructor(title, author, isbn) {
     this.title = title;
@@ -9,42 +8,46 @@ class Book {
 }
 
 // UI Class: handle UI task
-
 class UI {
   static displayBooks() {
-    const storedBooks = [
-      {
-        title: "Book One",
-        author: "John Doe",
-        isbn: "3434434",
-      },
-      {
-        title: "Book two",
-        author: "Jane Doe",
-        isbn: "323232",
-      },
-    ];
-
-    const books = storedBooks;
-
-    books.forEach((book) => UI.addbookToList(book));
+    const books = Store.getBooks();
+    books.forEach((book) => UI.addBookToList(book));
   }
 
-  static addbookToList(book) {
+  static addBookToList(book) {
     const list = document.querySelector("#book-list");
 
     const row = document.createElement("tr");
     row.innerHTML = ` 
-        <td>${book.title} </td>
-        <td>${book.author} </td>
-        <td>${book.isbn} </td>
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
         <td>
-        <a href = "#" class= "btn btn-danger btn-sm delete">X</a>
+        <a href="#" class="btn btn-danger btn-sm delete">X</a>
         </td>
     `;
 
     list.appendChild(row);
   }
+
+  static deleteBook(el) {
+    if (el.classList.contains("delete")) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+
+  static showAlert(message, className) {
+    const div = document.createElement("div");
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".container");
+    const form = document.querySelector("#book-form");
+    container.insertBefore(div, form);
+
+    // Vanish in 3 seconds
+    setTimeout(() => document.querySelector(".alert").remove(), 3000);
+  }
+
   static clearFields() {
     document.querySelector("#title").value = "";
     document.querySelector("#author").value = "";
@@ -53,29 +56,65 @@ class UI {
 }
 
 // Storage Class: handles storage
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+
+    const updatedBooks = books.filter((book) => book.isbn !== isbn);
+
+    localStorage.setItem("books", JSON.stringify(updatedBooks));
+  }
+}
 
 // Event: display books
 document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
 // Event: add a book
 document.querySelector("#book-form").addEventListener("submit", (e) => {
-  // Prevent actual submit
   e.preventDefault();
 
-  //Get form values
   const title = document.querySelector("#title").value;
   const author = document.querySelector("#author").value;
   const isbn = document.querySelector("#isbn").value;
 
-  // Istantiate book
-  const book = new Book(title, author, isbn);
-  console.log(book);
+  if (title === "" || author === "" || isbn === "") {
+    UI.showAlert("Please, fill in all fields", "danger");
+  } else {
+    const book = new Book(title, author, isbn);
 
-  // Add Book to UI
-  UI.addbookToList(book);
-
-  // Clear field
-  UI.clearFields();
+    UI.addBookToList(book);
+    Store.addBook(book);
+    UI.showAlert("Book added", "success");
+    UI.clearFields();
+  }
 });
 
 // Event: remove a book
+document.querySelector("#book-list").addEventListener("click", (e) => {
+  // Remove book from UI
+  UI.deleteBook(e.target);
+
+  // Remove book from store
+  if (e.target.classList.contains("delete")) {
+    const isbn = e.target.parentElement.previousElementSibling.textContent;
+    Store.removeBook(isbn);
+    UI.showAlert("Book removed", "success");
+  }
+});
